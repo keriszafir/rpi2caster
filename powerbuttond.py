916b76273209f830b588e093931a8ac74d73bb9e
elegantandrogyne <elegant_androgyne@altergothic.pl>
2014-10-23 14:12:24 +0200

    reconfigured I/Os due to electronics rebuild...

--- powerbuttond.py
+++ powerbuttond.py
@@ -3,27 +3,10 @@ import RPi.GPIO as gpio
 import os, sys, time, signal
 
 # initial config, use BCM GPIO numbers
-photocellGPIO = 14
-buttonGPIO = 15
-ledGPIO = 4
-
-try:
-# Set up the GPIO for button and green LED:
-  gpio.setmode(gpio.BCM)
-  gpio.setwarnings(False)
-  gpio.setup(buttonGPIO, gpio.IN, pull_up_down = gpio.PUD_DOWN) # shutdown button
-  gpio.setup(ledGPIO, gpio.OUT) # green LED
-  gpio.output(ledGPIO,1)
-# Set up the machine cycle sensor (photocell) GPIO to be used with rpi2caster:
-  os.system('echo "%s" > /sys/class/gpio/export' % photocellGPIO) # BCM pin for photocell input
-  os.system('echo "in" > /sys/class/gpio/gpio%s/direction' % photocellGPIO) # input
-# Enable generating interrupts when the photocell becomes obscured AND lit up
-  os.system('echo "both" > /sys/class/gpio/gpio%s/edge' % photocellGPIO)
-
-except RuntimeError:
-
-  print("You must run this program as root!")
-  sys.exit()
+photocellGPIO = 17         # yellow - was gpio 14, pin 8 - let's leave that alone for UART TxD
+shutdownbuttonGPIO = 22    # brown - was gpio 15, pin 10 - let's leave that alone for UART RxD
+rebootbuttonGPIO = 23      # black
+ledGPIO = 4                # blue
 
 def blink(n,speed):
   for i in range(0,n):
@@ -39,23 +22,48 @@ def signal_handler(signal, frame):
 # turn the green LED off if you stop the program with ctrl-C
     gpio.output(ledGPIO,0)
   gpio.cleanup()
-  os.system('echo "%s" > /sys/class/gpio/unexport' % photocellGPIO)
+  os.system('echo "%i" > /sys/class/gpio/unexport' % photocellGPIO)
   sys.exit()
 
-def shutdown():
+def shutdown(buttonGPIO, mode):
+  command = [0 : 'poweroff', 1 : 'reboot', 2: 'echo "debug info"']
   time.sleep(1)
   if (gpio.input(buttonGPIO) == 1):   #check if you're still pressing the button after 1sec
     blink(5,0.1)
-    os.system("poweroff")
+    os.system(command[mode])
     gpio.output(ledGPIO,1) # keep the green LED lit up until system shuts down completely
     gpio.cleanup()
-    os.system('echo "%s" > /sys/class/gpio/unexport' % photocellGPIO)
+    os.system('echo "%i" > /sys/class/gpio/unexport' % photocellGPIO)
     sys.exit()
+
+try:
+# Set up the GPIO for button and green LED:
+  gpio.setmode(gpio.BCM)
+  gpio.setwarnings(False)
+  gpio.setup(shutdownbuttonGPIO, gpio.IN, pull_up_down = gpio.PUD_DOWN) # shutdown button
+  gpio.setup(rebootbuttonGPIO, gpio.IN, pull_up_down = gpio.PUD_DOWN) # reboot button
+  gpio.setup(ledGPIO, gpio.OUT) # green LED
+  gpio.output(ledGPIO,1)
+# Set up the machine cycle sensor (photocell) GPIO to be used with rpi2caster:
+  os.system('echo "%i" > /sys/class/gpio/export' % photocellGPIO) # define BCM pin no for photocell input
+  os.system('echo "in" > /sys/class/gpio/gpio%i/direction' % photocellGPIO) # set gpio up as input
+# Enable generating interrupts when the photocell becomes obscured AND lit up
+  os.system('echo "both" > /sys/class/gpio/gpio%i/edge' % photocellGPIO)
+
+except RuntimeError:
+
+  print("You must run this program as root!")
+  sys.exit()
+
 
 signal.signal(signal.SIGINT, signal_handler)
 signal.signal(signal.SIGTERM, signal_handler)
-# gpio.add_event_detect(buttonGPIO, gpio.RISING, callback = shutdown, bouncetime = 1000)
+
+# If user presses shutdown or reboot button, do a threaded callback to shutdown function:
+gpio.add_event_detect(shutdownbuttonGPIO, gpio.RISING,
+    callback = shutdown(shutdownbuttonGPIO, 0), bouncetime = 1000)
+gpio.add_event_detect(rebootbuttonGPIO, gpio.RISING,
+    callback = shutdown(rebootbuttonGPIO, 1), bouncetime = 1000)
 
 while True:
-  gpio.wait_for_edge(buttonGPIO, gpio.RISING)  # hold the program execution until interrupt
-  shutdown()
+  time.sleep(1)  # just wait
