2a7f3c3e3409c6c9ff81fc810c8e349207219e97
elegantandrogyne <elegant_androgyne@altergothic.pl>
2014-10-27 20:47:51 +0100

    Moved printing the comments out of the parser, and restored the reverse-reading of file when casting. Lots of changes will follow, with moving all hardware-related stuff to a class

--- rpi2caster.py
+++ rpi2caster.py
@@ -14,47 +14,66 @@ import wiringpi2 as wiringpi
 
 
 
-def input_setup():
-  """ We need to set up the sysfs interface before (powerbuttond.py -
-  a daemon running on boot with root privileges takes care of it).
-  In the future,  we'll add configurable GPIO numbers. Why store the
-  device config in the program source, if we can use a .conf file?"""
-  photocellGPIO = 17
-  gpioSysfsPath = '/sys/class/gpio/gpio%s/' % photocellGPIO
-  global valueFileName
-  valueFileName = gpioSysfsPath + 'value'
-  edgeFileName = gpioSysfsPath + 'edge'
+class Hardware(object):
 
-  """Check if the photocell GPIO has been configured:"""
-  if not os.access(valueFileName, os.R_OK):
-    print('%s: file does not exist or cannot be read. '
-       'You must export the GPIO no %i as input first!'
-                      % (valueFileName, photocellGPIO))
-    exit()
-  """Check if the interrupts are generated for photocell GPIO
-  for both rising and falling edge:"""
-  with open(edgeFileName, 'r') as edgeFile:
-    if (edgeFile.read()[:4] != 'both'):
-      print('%s: file does not exist, cannot be read or the interrupt '
-      'on GPIO no %i is not set to "both". Check the system config.'
-      % (edgeFileName, photocellGPIO))
+  def __init__(self, photocellGPIO, mcp0Address, mcp1Address, pinBase):
+    self.photocellGPIO = photocellGPIO
+    self.mcp0Address = mcp0Address
+    self.mcp1Address = mcp1Address
+    self.pinBase = pinBase
+
+
+  def input_setup(photocellGPIO):
+    """Input configuration
+    We need to set up the sysfs interface before (powerbuttond.py -
+    a daemon running on boot with root privileges takes care of it).
+    In the future,  we'll add configurable GPIO numbers. Why store the
+    device config in the program source, if we can use a .conf file?"""
+
+    gpioSysfsPath = '/sys/class/gpio/gpio%s/' % photocellGPIO
+    global valueFileName
+    valueFileName = gpioSysfsPath + 'value'
+    edgeFileName = gpioSysfsPath + 'edge'
+
+    """Check if the photocell GPIO has been configured:"""
+    if not os.access(valueFileName, os.R_OK):
+      print('%s: file does not exist or cannot be read. '
+         'You must export the GPIO no %i as input first!'
+                        % (valueFileName, photocellGPIO))
       exit()
+    """Check if the interrupts are generated for photocell GPIO
+    for both rising and falling edge:"""
+    with open(edgeFileName, 'r') as edgeFile:
+      if (edgeFile.read()[:4] != 'both'):
+        print('%s: file does not exist, cannot be read or the interrupt '
+        'on GPIO no %i is not set to "both". Check the system config.'
+        % (edgeFileName, photocellGPIO))
+        exit()
 
 
+  def output_setup(mcp0Address, mcp1Address, pinBase):
+    """Output configuration
+    Setup the wiringPi MCP23017 chips for valve outputs:"""
+    wiringpi.mcp23017Setup(pinBase, mcp0Address)
+    wiringpi.mcp23017Setup(pinBase + 16, mcp1Address)
+    pins = range(pinBase, pinBase + 32)
+    signals = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11',
+             '12', '13', '14', '0005', '0075', 'A', 'B', 'C', 'D', 'E',
+             'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'S', 'O15']
 
-def output_setup():
-  """Setup the wiringPi MCP23017 chips for valve outputs:"""
-  wiringpi.mcp23017Setup(65,0x20)
-  wiringpi.mcp23017Setup(81,0x21)
-  for pin in range(65,97):
-    wiringpi.pinMode(pin,1)
-  """Assign wiringPi pin numbers on MCP23017s to the Monotype control codes."""
-  global wiringPiPinNumber
-  wiringPiPinNumber = dict([('1', 65), ('2', 66), ('3', 67), ('4', 68), ('5', 69),
-    ('6', 70), ('7', 71), ('8', 72), ('9', 73), ('10', 74), ('11', 75), ('12', 76),
-    ('13', 77), ('14', 78), ('0005', 79), ('0075', 80), ('A', 81), ('B', 82),
-    ('C', 83), ('D', 84), ('E', 85), ('F', 86), ('G', 87), ('H', 88), ('I', 89),
-    ('J', 90), ('K', 91), ('L', 92), ('M', 93), ('N', 94), ('S', 95), ('O15', 96)])
+    for pin in pins:
+      wiringpi.pinMode(pin,1)
+
+    """Assign wiringPi pin numbers on MCP23017s to the Monotype control codes."""
+    global wiringPiPinNumber
+        wiringPiPinNumber = dict(zip(signals, pins))
+
+    """old hard-wired assignment
+    wiringPiPinNumber = dict([('1', 65), ('2', 66), ('3', 67), ('4', 68), ('5', 69),
+      ('6', 70), ('7', 71), ('8', 72), ('9', 73), ('10', 74), ('11', 75), ('12', 76),
+      ('13', 77), ('14', 78), ('0005', 79), ('0075', 80), ('A', 81), ('B', 82),
+      ('C', 83), ('D', 84), ('E', 85), ('F', 86), ('G', 87), ('H', 88), ('I', 89),
+      ('J', 90), ('K', 91), ('L', 92), ('M', 93), ('N', 94), ('S', 95), ('O15', 96)])"""
 
 
 
@@ -92,25 +111,25 @@ def menu():
   ans = ''
   while ans == '':
     print ("""
-		Main menu:
+\t Main menu:
 
-		1. Load a ribbon file
+\t 1. Load a ribbon file
 
-		2. Cast type from ribbon file
+\t 2. Cast type from ribbon file
 
-		3. Punch a paper tape
+\t 3. Punch a paper tape
 
-		4. Cast sorts
+\t 4. Cast sorts
 
-		5. Test the valves and pinblocks
+\t 5. Test the valves and pinblocks
 
-		6. Lock the caster on a specified diecase position
+\t 6. Lock the caster on a specified diecase position
 
 
 
-		0. Exit to shell
+\t 0. Exit to shell
 
-		""")
+""")
 
     if inputFileName != '':
       print('Input file name: %s\n' % os.path.realpath(inputFileName))
@@ -144,7 +163,7 @@ def activate_valves(signals):
   in an array fed to the function. The input array "signals" can contain
   lowercase (a, b, g, s...) or uppercase (A, B, G, S...) descriptions.
   Do nothing if the function receives an empty sequence."""
-  if signals is not None and len(signals) != 0:
+  if len(signals) != 0:
     for monotypeSignal in signals:
       pin = wiringPiPinNumber[monotypeSignal]
       wiringpi.digitalWrite(pin,1)
@@ -183,18 +202,37 @@ def cast_composition(filename):
 
   """Open a file with signals"""
   with open(filename, 'rb') as ribbon:
+    contents = ribbon.readlines()
 
-    """Wait until the operator confirms"""
-    print('\nThe combinations of Monotype signals will be displayed '
-                     'on screen while the machine casts the type.\n')
-    raw_input('\nInput file found. Press return to start casting.\n')
-    for line in ribbon:
 
-      """Parse the row, return a list of signals, or None if there's
-      only a comment in the row"""
-      signals = signals_parser(line)
-      if signals is not None:
-        send_signals_to_caster(signals, 5)
+  """For casting, we need to read the file backwards"""
+  contents = reversed(contents)
+
+  """Wait until the operator confirms"""
+  print('\nThe combinations of Monotype signals will be displayed '
+                   'on screen while the machine casts the type.\n')
+  raw_input('\nInput file found. Press return to start casting.\n')
+  for line in contents:
+
+    """Parse the row, return a list of signals, or None if there's
+    only a comment in the row"""
+    outcome = signals_parser(line)
+
+    """The outcome is a list: [signals (list), comment (str)]
+    Both can be empty"""
+    signals = outcome[0]
+    comment = outcome[1]
+
+    """Print a comment if there is one"""
+    if len(comment) > 0:
+      print comment
+
+    """Cast an empty line, signals with comment, signals with no comment.
+    Don't cast a line with comment alone."""
+    if len(comment) == 0 or len(signals) > 0:
+      if len(signals) > 0:
+        print ' '.join(signals)
+      send_signals_to_caster(signals, 5)
 
   """After punching is finished, notify the user:"""
   raw_input('\nCasting finished. Press return to go to main menu. ')
@@ -220,11 +258,25 @@ def punch_composition(filename):
 
       """Parse the row, return a list of signals, or None if there's
       only a comment in the row"""
-      signals = signals_parser(line)
-      if signals is not None:
+      outcome = signals_parser(line)
+
+      """The outcome is a list: [signals (list), comment (str)]
+      Both can be empty"""
+      signals = outcome[0]
+      comment = outcome[1]
+
+      """Print a comment if there is one"""
+      if len(comment) > 0:
+        print comment
+
+      """Punch an empty line, signals with comment, signals with no comment.
+      Don't cast a line with comment alone."""
+      if len(comment) == 0 or len(signals) > 0:
+
         """Determine if we need to turn O+15 on"""
         if len(signals) < 2:
           signals += ('O15',)
+        print ' '.join(signals)
         activate_valves(signals)
 
         """The pace is arbitrary, let's set it to 200ms/200ms"""
@@ -262,22 +314,30 @@ def cast_sorts():
   Ask user about the diecase row & column, as well as number of sorts."""
   os.system('clear')
   print('Calibration and Sort Casting:\n\n')
-  signals = raw_input('Enter column and row symbols (default: G 5) ')
+  signals = raw_input('Enter column and row symbols (default: G 5, spacebar if O-15) ')
   print('\n')
   if signals == '':
     signals = 'G5'
-  signals = signals_parser(signals)
-  if signals is None:
+  outcome = signals_parser(signals)
+
+  """We need only a sequence of signals, no comments"""
+  combination = outcome[0]
+
+  """O15 yields no signals, but we may want to cast it - check if we
+  entered spacebar"""
+  if len(combination) == 0 and signals != ' ':
     print('\nRe-enter the sequence')
     time.sleep(1)
     cast_sorts()
   n = raw_input('\nHow many sorts do you want to cast? (default: 10) ')
   print('\n')
 
+  """Default to 10 if user enters non-positive number or letters"""
   if not n.isdigit() or int(n) < 0:
     n = '10'
   n = int(n)
 
+  """Warn if we want to cast too many sorts from a single matrix"""
   print ('\nWe\'ll cast it %i times.\n' % (n))
   if n > 30:
     print('\nWarning: you want to cast a single character more than '
@@ -295,8 +355,9 @@ def cast_sorts():
       print('Casting characters...')
       """Cast n combinations of row & column, one by one"""
       for i in range(n):
-        print ' '.join(signals)
-        send_signals_to_caster(signals, 5)
+        if len(combination) > 0:
+          print ' '.join(combination)
+        send_signals_to_caster(combination, 5)
       """After casting sorts we need to stop the pump"""
       print('Stopping pump and putting line to the galley...')
       send_signals_to_caster(['0005', '0075'], 5)
@@ -335,7 +396,8 @@ def lock_on_position():
   signals = ''
   while signals == '':
     signals = raw_input('Enter the signals to send to the machine: ')
-  combination = signals_parser(signals)
+  combination = signals_parser(signals)[0]
+  print ' '.join(combination)
   activate_valves(combination)
 
   """Wait until user decides to stop sending those signals to valves:"""
@@ -358,16 +420,17 @@ def signals_parser(originalSignals):
   combination. If a line in file contains a comment only, we cast nothing.
   So, we need an empty line if we want to cast O15 (place the comment above)."""
   commentSymbols = ['**', '*', '//', '##', '#']
+  comment = ''
   for symbol in commentSymbols:
     symbolPosition = signals.find(symbol)
     if symbolPosition != -1:
       comment = signals[symbolPosition + len(symbol):].strip()
       signals = signals[:symbolPosition].strip()
       signals = signals.upper()
-      print comment
-      """Return None if there's only a comment in the line."""
-      if signals == '':
-        return None
+      #print comment
+      #"""Return None if there's only a comment in the line."""
+      #if signals == '':
+      #  return None
 
   """Filter out all non-alphanumeric characters and whitespace"""
   signals = filter(str.isalnum, signals).upper()
@@ -399,39 +462,40 @@ def signals_parser(originalSignals):
   (S was taken care of earlier). That will be the column signals."""
   columns = filter(lambda s: s in list('ABCDEFGHIJKLMN'), list(signals))
 
-  """Join all the signal lists into one, print signals to console
-  (so that we know what we cast/punch) and return the result"""
+  """Join all the signal lists into one and return a list containing
+  the signals, as well as a comment."""
   resultSignals = columns + rows + special_signals
-  print(' '.join(resultSignals))
-  return resultSignals
+  #print(' '.join(resultSignals))
+  return [resultSignals, comment]
 
 
 
 def send_signals_to_caster(signals, machineTimeout):
   """Casting - the pace is dictated by the machine (via photocell)."""
-  if signals is not None:
-    with open(valueFileName, 'r') as gpiostate:
-      po = select.epoll()
-      po.register(gpiostate, select.POLLPRI)
-      previousState = 0
-      """Detect events on a photocell input and cast all signals in a row.
-      Ask the user what to do if the machine is stopped (no events)."""
-      while 1:
-        events = po.poll(machineTimeout)
-        if events:
-          """be sure that the machine is working"""
-          gpiostate.seek(0)
-          photocellState = int(gpiostate.read())
-          if photocellState == 1:
-            activate_valves(signals)
-            previousState = 1
-          elif photocellState == 0 and previousState == 1:
-            deactivate_valves()
-            previousState = 0
-            break
-        else:
-          """if machine isn't working, notify the user"""
-          machine_stopped()
+  with open(valueFileName, 'r') as gpiostate:
+    po = select.epoll()
+    po.register(gpiostate, select.POLLPRI)
+    previousState = 0
+    """Detect events on a photocell input and cast all signals in a row.
+    Ask the user what to do if the machine is stopped (no events)."""
+    while 1:
+      events = po.poll(machineTimeout)
+      if events:
+        """be sure that the machine is working"""
+        gpiostate.seek(0)
+        photocellState = int(gpiostate.read())
+        if photocellState == 1:
+          if len(signals) == 0:
+            print('O+15 - no signals')
+          activate_valves(signals)
+          previousState = 1
+        elif photocellState == 0 and previousState == 1:
+          deactivate_valves()
+          previousState = 0
+          break
+      else:
+        """if machine isn't working, notify the user"""
+        machine_stopped()
 
 
 
