7c9bf39b9ecd2d4e4cf5f99731a700ae7bb4e0f2
elegantandrogyne <krzysztof.slychan@gmail.com>
2015-02-22 16:10:51 +0100

    Removed a file used for testing the assertions in "imported" mode

--- debug-test-types.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python
-import rpi2caster
-
-session = rpi2caster.Session(
-                             job=rpi2caster.RibbonPunching(),
-                             database=rpi2caster.Database('database/monotype.db'),
-                             UI=rpi2caster.TextUI(debugMode=True),
-                             caster=rpi2caster.Keyboard()
-                            )
