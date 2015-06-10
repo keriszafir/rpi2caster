bed1d324a19db7c82eadaee2d196cf8d3f19a5f2
elegantandrogyne <krzysztof.slychan@gmail.com>
2015-06-10 11:16:15 +0200

    Working on diecase storage/retrieval/parsing functions module - matrix_data.py

--- rpi2caster/inventory.py
+++ rpi2caster/inventory.py
@@ -23,35 +23,6 @@ def show_diecase_layout():
     pass
 
 
-def lookup_diecase(type_series, type_size):
-    """lookup_diecase
-
-    Searches for a diecase of given type series (e.g. 327) and size (e.g. 12),
-    if several matches found - allows to choose one of them, returns data.
-    """
-    matches = DB.diecase_by_series_and_size(type_series, type_size)
-    if not matches:
-    # List is empty. Notify the user:
-        ui.display('Sorry - no results found.')
-        return False
-    elif len(matches) == 1:
-    # One result found
-        return matches[0]
-    else:
-    # More than one match - decide which one to use:
-        idents = [record[0] for record in matches]
-    # Associate diecases with IDs to select one later
-        assoc = dict(zip(idents, matches))
-    # Display a menu with diecases from 1 to the last:
-        options = dict([(i, k) for i, k in enumerate(matches, start=1)])
-        header = 'Choose a diecase:'
-        choice = ui.menu(options, header)
-    # Choose one
-        chosen_id = options[choice]
-    # Return a list with chosen diecase's parameters:
-        return assoc[chosen_id]
-
-
 def add_diecase():
     """Not implemented yet"""
     pass
