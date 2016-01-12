2f6ace5dcf4b747039f40ff3ed297d2525ce43af
elegantandrogyne <krzysztof.slychan@gmail.com>
2016-01-12 07:34:28 +0100

    Database update, new wedges schema

--- rpi2caster/inventory.py
+++ rpi2caster/inventory.py
@@ -45,26 +45,26 @@ def matrix_menu():
         """Exits to main menu"""
         exceptions.return_to_menu()
 
-    header = ('\nMatrix case manipulation menu:\n\n'
-              '[G]et matrix case parameters\n'
-              '[S]how matrix case layout\n'
-              '[E]dit matrix case layout\n'
-              '[C]lear matrix case layout\n'
-              '[L]oad matrix case layout from file\n'
-              '[D]elete the matrix case\n\n'
-              '[Enter] to choose another diecase\n'
-              '[M] to return to main menu\n\n'
-              'Your choice? : ')
-    options = {'S': matrix_data.show_diecase,
-               'L': matrix_data.load_layout,
-               'E': matrix_data.edit_diecase,
-               'C': matrix_data.clear_diecase,
-               'D': matrix_data.delete_diecase,
-               'G': matrix_data.get_diecase_parameters,
-               '': another,
-               'M': menu}
-    while True:
-        diecase_id = matrix_data.choose_diecase()[0]
+    def work_on_diecase():
+        """Options for working on a chosen diecase"""
+        header = ('\nMatrix case manipulation menu:\n\n'
+                  '[G]et matrix case parameters\n'
+                  '[S]how matrix case layout\n'
+                  '[E]dit matrix case layout\n'
+                  '[C]lear matrix case layout\n'
+                  '[L]oad matrix case layout from file\n'
+                  '[D]elete the matrix case\n\n'
+                  '[Enter] to choose another diecase\n'
+                  '[M] to return to main menu\n\n'
+                  'Your choice? : ')
+        options = {'S': matrix_data.show_diecase,
+                   'L': matrix_data.load_layout,
+                   'E': matrix_data.edit_diecase,
+                   'C': matrix_data.clear_diecase,
+                   'D': matrix_data.delete_diecase,
+                   'G': matrix_data.get_diecase_parameters,
+                   '': another,
+                   'M': menu}
         try:
             while True:
                 ui.display('\nNow working on diecase %s' % diecase_id)
@@ -74,6 +74,10 @@ def matrix_menu():
                 ui.confirm('[Enter] to continue...')
         except exceptions.MenuLevelUp:
             pass
+
+    while True:
+        diecase_id = matrix_data.choose_diecase()[0]
+        work_on_diecase()
 
 
 def wedge_menu():
