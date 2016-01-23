fd748f6c2c1c49298537e085b63f8b630ac17c80
elegantandrogyne <krzysztof.slychan@gmail.com>
2016-01-23 10:05:41 +0100

    Making it into a package

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='rpi2caster',
+      version='0.1',
+      description='Raspberry Pi controls a Monotype composition caster',
+      url='http://github.com/elegantandrogyne/rpi2caster',
+      author='Christophe Slychan',
+      author_email='krzysztof.slychan@gmail.com',
+      license='GPLv3',
+      packages=['rpi2caster'],
+      zip_safe=False)
