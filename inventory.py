52520ccff71888b850bf76b8a55376e3e69d0fa2
elegantandrogyne <krzysztof.slychan@gmail.com>
2016-01-31 17:01:02 +0100

    Remove old scripts - no longer needed after switch to single-script

--- inventory.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-"""Inventory
-
-Script for managing diecases and wedges.
-"""
-from rpi2caster import inventory
-inventory.main_menu()
--- typesetter.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-"""Typesetter script
-
-Used for running the typesetter program.
-"""
-from rpi2caster import typesetter
-typesetter.main_menu()
