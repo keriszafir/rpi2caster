273df4f59818a49e07e213532d8477660f6c680c
elegantandrogyne <krzysztof.slychan@gmail.com>
2016-02-13 20:16:11 +0100

    Remove build directory (added accidentally), update .gitignore

--- build/lib/rpi2caster/__main__.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""rpi2caster - top-level script for starting the components"""
-from .rpi2caster import main
-
-if __name__ == '__main__':
-    main()
