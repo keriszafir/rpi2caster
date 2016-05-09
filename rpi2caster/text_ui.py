eb731f230f43665c76ae7eb38550eb037a4c8bbd
elegantandrogyne <krzysztof.slychan@gmail.com>
2016-05-09 16:12:01 +0200

    Diecase layout column stretch for long values (avoid corrupting the table)

--- rpi2caster/text_ui.py
+++ rpi2caster/text_ui.py
@@ -269,7 +269,7 @@ def confirm(question, default=None):
 def display_diecase_layout(diecase):
     """Shows a layout for a given diecase ID, unit values for its
     assigned wedge, or the typical S5 if not specified."""
-    def displayed_char(matrix):
+    def get_char(matrix):
         """Modifies matrix char for displaying"""
         # Style modifiers for displaying roman, bold, italic,
         # smallcaps, inferior, superior
@@ -281,44 +281,60 @@ def display_diecase_layout(diecase):
             fmt = '#'
         return spaces_symbols.get(matrix.char, fmt + matrix.char)
 
+    def get_width(column):
+        """Get the column width"""
+        return column_widths.get(column, 4)
+
+    def get_unit_value(mat):
+        """Get the unit value string for a character, if the width
+        overrides the wedge default"""
+        show_units = (mat.units != wedge_row_units and
+                      mat.char and not mat.isspace())
+        if show_units:
+            return str(mat.units)
+        else:
+            return ''
+
     col_numbers = c.COLUMNS_15
     row_numbers = [x for x in range(1, 16)]
+    # Set the initial column width value to 4
+    column_widths = {}
+    # Now loop over the matrices and adjust the displayed table
     for mat in diecase:
         if '16' in mat.code:
             col_numbers = c.COLUMNS_17
             row_numbers = [x for x in range(1, 17)]
             break
-        elif 'NI' in mat.code or 'NL' in mat.code:
+        elif mat.column in ('NI', 'NL'):
             col_numbers = c.COLUMNS_17
+        # Update column width if displayed character turns out too long
+        if len(get_char(mat)) + 2 > column_widths.get(mat.column, 4):
+            column_widths[mat.column] = len(get_char(mat)) + 2
     # Generate a header with column numbers
-    header = (' Row ' + ''.join([col.center(4) for col in col_numbers]) +
-              'Units'.center(9))
+    # Widths are variable and depend on text field width
+    fields = [col.center(get_width(col)) for col in col_numbers]
+    header = ' Row %s Units ' % ''.join(fields)
     # "-----" line in the table
     separator = '—' * len(header)
     # Initialize the displayed layout
     table = [header, separator]
     # Process each row
-    for row_number in row_numbers:
-        row = []
+    for row in row_numbers:
+        items = []
         units_row = []
-        wedge_row_units = diecase.wedge[row_number]
-        for column_number in col_numbers:
+        wedge_row_units = diecase.wedge[row]
+        for col in col_numbers:
+            width = get_width(col)
             for mat in diecase:
-                if mat.code == '%s%s' % (column_number, row_number):
-                    row.append(displayed_char(mat).center(4))
-                    # Show units under character if value is not row default
-                    show_units = (mat.units != wedge_row_units and
-                                  mat.char and not mat.isspace())
-                    if show_units:
-                        units_row.append(str(mat.units).center(4))
-                    else:
-                        units_row.append(''.center(4))
+                if mat.code == '%s%s' % (col, row):
+                    items.append(get_char(mat).center(width))
+                    units_row.append(get_unit_value(mat).center(width))
                     # No need to iterate further
                     break
-        row = ['|', str(row_number).center(3), '|', ''.join(row),
-               '|%s|' % str(wedge_row_units).center(7)]
-        units_row = ['|', ' ' * 3, '|', ''.join(units_row), '|', ' ' * 7, '|']
-        table.append(''.join(row))
+        items = ['|', str(row).center(3), '|', ''.join(items),
+                 '|%s|' % str(wedge_row_units).center(5)]
+        units_row = ['|', ' ' * 3, '|', ''.join(units_row), '|', ' ' * 5, '|']
+        table.append(''.join(items))
         table.append(''.join(units_row))
         # table.append(empty_row)
     # Add the header at the bottom
