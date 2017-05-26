16747c92d912aa4f794e35bcf5ba550dc3066004
elegantandrogyne <krzysztof.slychan@gmail.com>
2017-05-26 12:04:50 +0200

    rearrange __init__.py - to match sequence of imports

--- rpi2caster/__init__.py
+++ rpi2caster/__init__.py
@@ -3,10 +3,23 @@
 -- specialized typesetting and machine control software
 for Monotype composition casting machines."""
 
-from . import basic_models, basic_controllers, casting_models, config, data
-from . import datatypes, definitions, matrix_controller, misc, main_models
-from . import monotype, parsing, rpi2caster, typesetting, typesetting_funcs, ui
+from . import misc
+from . import config
+from . import data
+from . import datatypes
+from . import definitions
+from . import ui
+from . import parsing
+from . import typesetting_funcs
+from . import basic_models
+from . import casting_models
+from . import main_models
+from . import basic_controllers
+from . import matrix_controller
+from . import typesetting
+from . import monotype
 from . import utilities
+from . import rpi2caster
 
 __all__ = [basic_models, basic_controllers, casting_models, config, data,
            datatypes, definitions, main_models, matrix_controller, misc,
