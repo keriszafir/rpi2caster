d4a72f2e1b1d5c26cfa84b855712e36908b3c138
elegantandrogyne <krzysztof.slychan@gmail.com>
2017-05-26 12:53:36 +0200

    __init__ rework

--- rpi2caster/__init__.py
+++ rpi2caster/__init__.py
@@ -1,37 +1,5 @@
 # -*- coding: utf-8 -*-
-"""rpi2caster:
--- specialized typesetting and machine control software
-for Monotype composition casting machines."""
-
-from . import misc
-from . import config
-from . import data
-from . import datatypes
-from . import definitions
-from . import ui
-from . import parsing
-from . import typesetting_funcs
-from . import basic_models
-from . import casting_models
-from . import main_models
-from . import basic_controllers
-from . import matrix_controller
-from . import typesetting
-from . import monotype
-from . import utilities
-from . import rpi2caster
-
-__all__ = [basic_models, basic_controllers, casting_models, config, data,
-           datatypes, definitions, main_models, matrix_controller, misc,
-           monotype, parsing, rpi2caster, typesetting, typesetting_funcs, ui,
-           utilities]
-
-
-__version__ = '0.7.dev1'
-
-__author__ = 'Krzysztof Słychań'
-
-__doc__ = """
+"""
     rpi2caster is a CAT (computer-aided typesetting) software
     for the Monotype composition caster (a hot-metal typesetting machine).
 
@@ -48,8 +16,22 @@ __doc__ = """
     for calibrating and testing the machine and control interface.
 
     For usage info, run `rpi2caster --help`
-    """
+
+"""
+
+from .rpi2caster import main
+
+MODULES = ['basic_models', 'basic_controllers', 'casting_models', 'config',
+           'data', 'datatypes', 'definitions', 'main_models',
+           'matrix_controller', 'misc', 'monotype', 'parsing', 'rpi2caster',
+           'typesetting', 'typesetting_funcs', 'ui', 'utilities']
+
+__all__ = MODULES
+
+__version__ = '0.7.dev1'
+
+__author__ = 'Krzysztof Słychań'
 
 
 if __name__ == '__main__':
-    rpi2caster.main()
+    main()
