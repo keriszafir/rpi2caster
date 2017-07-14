f5ddc95165cff1a22f0e1d9d8fc944ed5006c672
elegantandrogyne <krzysztof.slychan@gmail.com>
2017-07-14 09:33:00 +0200

    push to PyPI

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
 from setuptools import setup, find_packages
 
 # global package-wide declarations
-__version__ = '0.7.beta1'
+__version__ = '0.7.beta2'
 __author__ = 'Christophe Slychan'
 __author_email__ = 'krzysztof.slychan@gmail.com'
 __github_url__ = 'http://github.com/elegantandrogyne/rpi2caster'
