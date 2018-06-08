fb7167c32aeb0f634171b490fc9761b138643195
elegantandrogyne <krzysztof.slychan@gmail.com>
2018-06-08 14:29:40 +0200

    fix version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
 from setuptools import setup, find_packages
 
 # global package-wide declarations
-__version__ = '1.3.alpha2'
+__version__ = '1.04.alpha2'
 __author__ = 'Christophe Slychan'
 __author_email__ = 'krzysztof.slychan@gmail.com'
 __github_url__ = 'http://github.com/elegantandrogyne/rpi2caster'
